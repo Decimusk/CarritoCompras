5be1211ef6eb3c32b23166343886396a8566f41c
Guillermo Pérez <112012124+Decimusk@users.noreply.github.com>
2022-09-27 11:45:40 -0600

    Add files via upload

---
+++ MVCCarritoCompras/Repositories/RepositoryProducto.cs
@@ -0,0 +1,29 @@
+﻿using MVCCarritoCompras.Data;
+using MVCCarritoCompras.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCCarritoCompras.Repositories
+{
+    public class RepositoryProducto
+    {
+        ContextoProducto contexto;
+        public RepositoryProducto()
+        {
+            this.contexto = new ContextoProducto();
+        }
+        public List<Producto> GetProductos()
+        {
+            var consulta = from datos in contexto.Productos select datos;
+            return consulta.ToList();
+        }
+
+        public List<Producto> BuscarProductos(List<int> id)
+        {
+            var consulta = from datos in contexto.Productos where id.Contains(datos.IdProducto) select datos;
+            return consulta.ToList();
+        }
+    }
+}
